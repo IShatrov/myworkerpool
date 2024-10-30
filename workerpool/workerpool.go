b8e1454ff7d76cf8b0e750d07ecef079b965b33b
IShatrov <shatrov2004@mail.ru>
2024-10-30 19:50:46 +0300

    Added comments

--- workerpool/workerpool.go
+++ workerpool/workerpool.go
@@ -8,11 +8,14 @@ import (
 
 const bufferSize = 10
 
+
+// A primitive worker-pool implementation.
 type Workerpool struct {
 	quitChannels map[string]chan<- bool
 	data         chan string
 }
 
+// Creates a new Workerpool.
 func NewWorkerpool() *Workerpool {
 	quitChannels := make(map[string]chan<- bool)
 	data := make(chan string, bufferSize)
@@ -20,10 +23,12 @@ func NewWorkerpool() *Workerpool {
 	return &Workerpool{quitChannels, data}
 }
 
+// Reports whether a Workerpool is valid.
 func (workerpool *Workerpool) IsValid() bool {
 	return workerpool.quitChannels != nil && workerpool.data != nil
 }
 
+// Adds a worker to a Workerpool. sleepTime is used to simulate a long job.
 func (workerpool *Workerpool) AddWorker(id string, sleepTime time.Duration) error {
 	_, contains := workerpool.quitChannels[id]
 
@@ -39,6 +44,7 @@ func (workerpool *Workerpool) AddWorker(id string, sleepTime time.Duration) erro
 	return nil
 }
 
+// Deletes a worker from a Workerpool.
 func (workerpool *Workerpool) DeleteWorker(id string) error {
 	quit, contains := workerpool.quitChannels[id]
 
@@ -56,20 +62,25 @@ func (workerpool *Workerpool) DeleteWorker(id string) error {
 	return errors.New("Worker " + id + " does not exist")
 }
 
+// Reports whether a Workerpool has workers.
 func (workerpool *Workerpool) HasWorkers() bool {
 	return len(workerpool.quitChannels) != 0
 }
 
+// Adds a job to a Workerpool.
 func (workerpool *Workerpool) AddJob(job string) {
 	workerpool.data <- job
 }
 
+// Adds all jons in the slice to a Workerpool.
 func (workerpool *Workerpool) AddJobs(jobs []string) {
 	for _, job := range jobs {
 		workerpool.AddJob(job)
 	}
 }
 
+
+// Performs a job.
 func (workerpool *Workerpool) worker(id string, quit <-chan bool, src <-chan string, sleepTime time.Duration) {
 	for {
 		select {
