bb8a2672d1cc3c92e3700f24feaa1172476f0ae6
IShatrov <shatrov2004@mail.ru>
2024-10-30 17:14:11 +0300

    Added AddWorker() and AddJob()

--- workerpool/workerpool.go
+++ workerpool/workerpool.go
@@ -1,6 +1,7 @@
 package workerpool
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -23,10 +24,29 @@ func (workerpool *Workerpool) IsValid() bool {
 	return workerpool.quitChannels != nil && workerpool.data != nil
 }
 
+func (workerpool *Workerpool) AddWorker(id string, sleepTime time.Duration) error {
+	_, contains := workerpool.quitChannels[id]
+
+	if contains {
+		return errors.New("Pool already contains worker with id" + id)
+	}
+
+	quit := make(chan bool)
+	workerpool.quitChannels[id] = quit
+
+	go workerpool.worker(id, quit, workerpool.data, sleepTime)
+
+	return nil
+}
+
+func (workerpool *Workerpool) AddJob(job string) {
+	workerpool.data <- job
+}
+
 func (workerpool *Workerpool) worker(id string, quit <-chan bool, src <-chan string, sleepTime time.Duration) {
 	for {
 		select {
-		case <- quit:
+		case <-quit:
 			fmt.Println(id)
 			return
 		default:
