0fa73a121676b2888d26823e841fe204cee20907
IShatrov <shatrov2004@mail.ru>
2024-10-30 17:23:12 +0300

    Added an example

--- example/example.go
+++ example/example.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"time"
 
+	"github.com/IShatrov/myworkerpool/workerpool"
+)
+
+func main() {
+	pool := workerpool.NewWorkerpool()
+
+	pool.AddJob("Emergency meeting")
+	pool.AddJob("Vote red")
+
+	pool.AddWorker("Amogus", time.Second)
+	pool.AddWorker("Impostor", time.Second)
+
+	pool.AddJob("Eject red")
+
+	time.Sleep(5 * time.Second)
 }
