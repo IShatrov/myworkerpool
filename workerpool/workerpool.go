878b53c2ae92dd9eda025c28a5ae7bab4f2b5383
IShatrov <shatrov2004@mail.ru>
2024-10-30 17:07:32 +0300

    Added worker()

--- workerpool/workerpool.go
+++ workerpool/workerpool.go
@@ -1,5 +1,10 @@
 package workerpool
 
+import (
+	"fmt"
+	"time"
+)
+
 const bufferSize = 10
 
 type Workerpool struct {
@@ -16,4 +21,19 @@ func NewWorkerpool() *Workerpool {
 
 func (workerpool *Workerpool) IsValid() bool {
 	return workerpool.quitChannels != nil && workerpool.data != nil
+}
+
+func (workerpool *Workerpool) worker(id string, quit <-chan bool, src <-chan string, sleepTime time.Duration) {
+	for {
+		select {
+		case <- quit:
+			fmt.Println(id)
+			return
+		default:
+			job := <-src
+			fmt.Printf("Worker \"%s\" about to perform job \"%s\"\n", id, job)
+			time.Sleep(sleepTime)
+			fmt.Printf("Worker \"%s\" finished job \"%s\"\n", id, job)
+		}
+	}
 }
